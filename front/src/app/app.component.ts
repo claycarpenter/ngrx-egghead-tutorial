bb156ab76f6a735299af56c86832f2066fdbf040
Clay Carpenter <claycarpenter@gmail.com>
2017-05-20 21:54:39 -0400

    Completes clock portion of tutorial.

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works on docker!';
+  click$ = new Subject();
+  clock;
+
+  constructor() {
+    this.clock = Observable
+      .merge(
+        this.click$.mapTo('hour'),
+        Observable.interval(1000).mapTo('second')
+      )
+      .scan((acc:Date, curr) => {
+        const date = new Date(acc.getTime());
+
+        if(curr === 'second'){
+          date.setSeconds(date.getSeconds() + 1);
+        }
+
+        if(curr === 'hour'){
+          date.setHours(date.getHours() + 1);
+        }
+
+        return date;
+      }, new Date());
+  }
 }
